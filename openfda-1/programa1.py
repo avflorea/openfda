0bd4325a8bff96cb2e961476cb0d52780cd79043
avflorea <andu.valentina99@gmail.com>
2018-03-23 17:41:06 +0100

    Practica 1

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -3,14 +3,7 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-conn = http.client.HTTPSConnection("https://api.fda.gov/drug/event.json?limit=1")
-conn.request("GET", "/users/avflorea/repos", None, headers)
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "https://api.fda.gov/drug/event.json?search=openfda:spl_id", None, headers)
 r1 = conn.getresponse()
-print(r1.status, r1.reason)
-repos_raw = r1.read().decode("utf-8")
-conn.close()
-
-repos = json.loads(repos_raw)
-
-repo = repos[0]
-print("The owner of the first repository is", repo['owner']['login'])
+print(r1)
