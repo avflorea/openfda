20150bd604b8e653d4a8187fc0c686635eb9debe
avflorea <andu.valentina99@gmail.com>
2018-03-23 18:29:33 +0100

    Practica 1

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -7,8 +7,10 @@ conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET", "/drug/label.json?search=results.openfda:spl_id&limit=1", None, headers)
 
 id = conn.getresponse()
-
+print(id.status, id.reason)
+data1 = id.read()
 for element in id:
     print(element)
 
-print(id)
+print(data1)
+conn.close()
