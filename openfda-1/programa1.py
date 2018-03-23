74e9bd5cf51fb962bdda51d4f414bc7845b55449
avflorea <andu.valentina99@gmail.com>
2018-03-23 18:18:00 +0100

    Practica 1

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -4,7 +4,7 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "https://api.fda.gov/drug/event.json?search=results.openfda:spl_id&limit=100", None, headers)
+conn.request("GET", "/drug/label.json?search=results.openfda:spl_id&limit=100", None, headers)
 
 id = conn.getresponse()
 
