64ca4a3fb48f95367ddb01b9bc880ece97e41790
avflorea <37366183+avflorea@users.noreply.github.com>
2018-03-24 17:35:07 +0100

    Práctica 1

--- openfda-1/programa1.py
+++ openfda-1/programa1.py
@@ -5,15 +5,16 @@ headers = {'User-Agent': 'http-client'}
 conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET", "/drug/label.json?&limit=20", None, headers)
 
-info = conn.getresponse()
+info = dict(conn.getresponse())
+# craear diccionario donde introducimos el json y de ahi leemos
 
 print(info.status, info.reason)
 repos_raw = info.read().decode("utf-8")
 conn.close()
 
-repos = json.loads(repos_raw)
+datos = json.loads(repos_raw)
 
-for element in info:
+for element in datos:
     print(element)
     print("El identificador es", element['results'][0]['id'])
     print("El proposito del producto es", element['results'][0]['purpose'])
