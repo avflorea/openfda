9565efa19aa739a4766d653fbe0b07cb7f7b3441
avflorea <37366183+avflorea@users.noreply.github.com>
2018-03-25 22:26:58 +0200

    Práctica 1

---
+++ openfda-1/programa2.py
@@ -0,0 +1,23 @@
+import http.client
+import json
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?&limit=20", None, headers)
+
+info = conn.getresponse()
+
+print(info)
+
+
+print(info.status, info.reason)
+
+repos_raw = info.read().decode("utf-8")
+
+datos = dict(json.loads(repos_raw))
+
+print(datos['results'][0]['id'])
+#for element in info:#datos['results'][0]['id']:
+ #   print(element)
+
+conn.close()
