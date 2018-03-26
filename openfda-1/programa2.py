948291038c9491e5786f8cb731edd15291dc55ae
avflorea <37366183+avflorea@users.noreply.github.com>
2018-03-26 13:42:20 +0200

    Programa 2 de Práctica 1

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -12,27 +12,11 @@ print(info)
 print(info.status, info.reason)
 repos_raw = info.read().decode("utf-8")
 
-datos = (json.loads(repos_raw))
+datos = json.loads(repos_raw)
 
 for element in datos['results']:
-    print(element, datos['results'][0]['id'])
+    print("El identificador es: ",element['id'])
 
 
-
-
-#for element in datos:
- #   print(element, ":", datos[element])
-  #  print(datos['results'][0]['id'])
-#datos2 = datos.keys()
-#datos3 = datos.values()
-
-
-#elements = datos.items()
-#for datos2,datos3 in datos.items():
- #   print(datos2,'-->',datos3)
-#for element in datos['results'][0]:
- #   print(element)
-
-#print(datos['results'][0]['id'])
 
 conn.close()
