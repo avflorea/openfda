cfb9885b60f5a53639bc83a7a1d639442c2089bb
avflorea <37366183+avflorea@users.noreply.github.com>
2018-04-01 21:01:13 +0200

    Programa aspirina de Práctica 2

--- openfda-2/programa-aspirina.py
+++ openfda-2/programa-aspirina.py
@@ -4,18 +4,18 @@ import json
 lista_drogas = []
 headers = {'User-Agent': 'http-client'}
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?search=(active_ingredient=acetylsalicylic)&limit=10", None, headers)
+conn.request("GET", '/drug/label.json?&search=active_ingredient:"acetylsalicylic+acid"&limit=100', None, headers)
 info = conn.getresponse()
 print(info.status, info.reason)
 drogas_raw = info.read().decode("utf-8")
 datos = (json.loads(drogas_raw))
+
 for elem in range(len(datos['results'])):
     info_drogas = datos['results'][elem]
     if (info_drogas['openfda']):
         lista_drogas.append(info_drogas['openfda']['manufacturer_name'][0])
-        print(lista_drogas)
+        print("Fabricantes que producen aspirinas son: ",lista_drogas)
     else:
         continue
-
 
 conn.close()
