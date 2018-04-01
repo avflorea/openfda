3226d02485df23ec5fdfd03102dc23deb24dbb3f
avflorea <37366183+avflorea@users.noreply.github.com>
2018-04-01 11:53:14 +0200

    Programa aspirina de Práctica 2

--- openfda-2/programa-aspirina.py
+++ openfda-2/programa-aspirina.py
@@ -1,28 +1,21 @@
 import http.client
 import json
+
+lista_drogas = []
 headers = {'User-Agent': 'http-client'}
-
 conn = http.client.HTTPSConnection("api.fda.gov")
-
-conn.request("GET", "/drug/label.json?&limit=100", None, headers)
-
+conn.request("GET", "/drug/label.json?search=(active_ingredient=acetylsalicylic)&limit=10", None, headers)
 info = conn.getresponse()
-
-print(info)
-
 print(info.status, info.reason)
-
-repos_raw = info.read().decode("utf-8")
-
-datos = json.loads(repos_raw)
-
-print(datos['results'][0]['openfda']['manufacturer_name'])
-if "aspirin" in datos:
-    #for element in datos['results']['openfda']:
-    print(datos['results'][0]['openfda']['manufacturer_name'])
-#for element in datos['results']:
-
- #   print(element, datos['results'][0]['id'])
+drogas_raw = info.read().decode("utf-8")
+datos = (json.loads(drogas_raw))
+for elem in range(len(datos['results'])):
+    info_drogas = datos['results'][elem]
+    if (info_drogas['openfda']):
+        lista_drogas.append(info_drogas['openfda']['manufacturer_name'][0])
+        print(lista_drogas)
+    else:
+        continue
 
 
 conn.close()
